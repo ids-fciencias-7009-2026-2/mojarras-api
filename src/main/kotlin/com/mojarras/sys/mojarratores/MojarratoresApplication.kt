9f1c8c563eb5b05447c952e3d1a2706bcf215f8c
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-02-17 12:57:36 -0600

    Configuracion

---
+++ src/main/kotlin/com/mojarras/sys/mojarratores/MojarratoresApplication.kt
@@ -0,0 +1,11 @@
+package com.mojarras.sys.mojarratores
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MojarratoresApplication
+
+fun main(args: Array<String>) {
+	runApplication<MojarratoresApplication>(*args)
+}
---
+++ src/test/kotlin/com/mojarras/sys/mojarratores/MojarratoresApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mojarras.sys.mojarratores
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MojarratoresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
