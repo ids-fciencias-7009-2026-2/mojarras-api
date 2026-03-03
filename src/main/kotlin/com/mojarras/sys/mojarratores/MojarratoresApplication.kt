7b0c5dc9d3b721fbf3c796774ecc54fee50dac07
md_cess <mdcesar136@gmail.com>
2026-03-02 23:45:46 -0600

    Practica 2

--- src/main/kotlin/com/mojarras/sys/mojarratores/MojarratoresApplication.kt
+++ src/main/kotlin/com/mojarras/sys/mojarratores/MojarratoresApplication.kt
@@ -1,5 +1,6 @@
 package com.mojarras.sys.mojarratores
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,8 @@ import org.springframework.boot.runApplication
 class MojarratoresApplication
 
 fun main(args: Array<String>) {
+	dotenv().entries().forEach {
+		System.setProperty(it.key, it.value)
+	}
 	runApplication<MojarratoresApplication>(*args)
 }
