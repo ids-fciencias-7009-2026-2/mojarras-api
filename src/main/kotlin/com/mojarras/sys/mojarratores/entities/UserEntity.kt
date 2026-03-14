91fa5f7c172770199aca5e6cf62a6821672ac2ec
MiguelAngel59 <miguel.mavt59@gmail.com>
2026-03-14 09:38:05 -0600

    feat: add UserRepository for persistence layer

--- src/main/kotlin/com/mojarras/sys/mojarratores/entities/UserEntity.kt
+++ src/main/kotlin/com/mojarras/sys/mojarratores/entities/UserEntity.kt
@@ -1,4 +1,4 @@
-package com.mojarras.sys.mojarratores.entity
+package com.mojarras.sys.mojarratores.entities
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
---
+++ src/main/kotlin/com/mojarras/sys/mojarratores/repositories/UserRepository.kt
@@ -0,0 +1,22 @@
+package com.mojarras.sys.mojarratores.repositories
+
+import com.mojarras.sys.mojarratores.entities.UserEntity
+import jakarta.transaction.Transactional
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+
+interface UserRepository : CrudRepository<UserEntity, Long> {
+
+    @Query("select u from UserEntity u where u.token = :token")
+    fun findByToken(token: String): UserEntity?
+
+    @Query("select u from UserEntity u where u.email = :email and u.password = :password")
+    fun findUserByPasswordAndEmail(email: String, password: String): UserEntity?
+
+    @Modifying
+    @Transactional
+    @Query("update UserEntity u set u.token = :token where u.id = :id")
+    fun updateTokenById(id: Long, token: String?)
+
+}
