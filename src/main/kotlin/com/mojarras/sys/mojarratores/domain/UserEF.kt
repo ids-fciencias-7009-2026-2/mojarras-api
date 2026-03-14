9822433e89d66b8860bfe0509c0d12df4eba5d5e
MiguelAngel59 <miguel.mavt59@gmail.com>
2026-03-14 09:58:29 -0600

    feat: add fun mapper UserEntity.toUser()

--- src/main/kotlin/com/mojarras/sys/mojarratores/domain/UserEF.kt
+++ src/main/kotlin/com/mojarras/sys/mojarratores/domain/UserEF.kt
@@ -1,6 +1,7 @@
 package com.mojarras.sys.mojarratores.domain
 
 import com.mojarras.sys.mojarratores.dto.request.CreateUserRequest
+import com.mojarras.sys.mojarratores.entities.UserEntity
 import java.util.UUID
 
 fun CreateUserRequest.toUser(): User {
@@ -12,5 +13,18 @@ fun CreateUserRequest.toUser(): User {
         firstName = this.firstName,
         lastName = this.lastName,
         zipCode = this.zipCode
+    )
+}
+
+fun UserEntity.toUser(): User {
+    return User(
+        id = this.id,
+        email = this.email,
+        password = this.password,
+        username = this.username,
+        firstName = this.firstName,
+        lastName = this.lastName,
+        zipCode = this.zipCode,
+        token = this.token
     )
 }
